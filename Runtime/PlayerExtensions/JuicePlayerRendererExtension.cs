3caa13dd293c76faff8485698448dd3d6083f72a
eliott <eliott.jaquier@gmail.com>
2026-03-06 21:29:33 +0100

    Add JuicePlayerRendererExtension for enhanced renderer management

---
+++ Runtime/PlayerExtensions/JuicePlayerRendererExtension.cs
@@ -0,0 +1,32 @@
+#nullable enable
+
+using UnityEngine;
+
+namespace JuiceVFX
+{
+    public class JuicePlayerRendererExtension : JuicePlayerExtension
+    {
+        [Tooltip("If true, searches for Renderers in all children of this GameObject. If false, only looks for a Renderer on this GameObject.")]
+        public bool searchInChildren = false;
+
+        private void OnEnable()
+        {
+            UpdateTargetRenderers();
+        }
+
+        private void UpdateTargetRenderers()
+        {
+            if (_juicePlayer == null) return;
+
+            if (searchInChildren)
+            {
+                _juicePlayer.targetRenderers = GetComponentsInChildren<Renderer>();
+            }
+            else
+            {
+                var singleRenderer = GetComponent<Renderer>();
+                _juicePlayer.targetRenderers = singleRenderer != null ? new[] { singleRenderer } : System.Array.Empty<Renderer>();
+            }
+        }
+    }
+}
